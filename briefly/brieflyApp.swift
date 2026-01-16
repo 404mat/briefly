45f4d21bc4082b5013fe7cad92ddbbf03e8b4032
Mathias D <mat.decaille@gmail.com>
2026-01-15 23:16:05 -0500

    Initial Commit

---
+++ briefly/brieflyApp.swift
@@ -0,0 +1,17 @@
+//
+//  brieflyApp.swift
+//  briefly
+//
+//  Created by Mathias on 2026-01-15.
+//
+
+import SwiftUI
+
+@main
+struct brieflyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
